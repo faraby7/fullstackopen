d923427212ede6b42796560a63e12b2cbcfa69d1
Youssef Faraby <yusuf.faraby@gmail.com>
2020-11-25 09:13:18 +0200

    1.1: course information, step1

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -10,7 +10,7 @@ const Header = (props) => {
 const Content = (props) => {
   return(
     <p>
-      {props.part} {props.exercice}
+      {props.part} {props.exercise}
     </p>
   )
 }
@@ -33,9 +33,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercice={exercises1} />
-      <Content part={part2} exercice={exercises2}/>
-      <Content part={part3} exercice={exercises3}/>
+      <Content part={part1} exercise={exercises1} />
+      <Content part={part2} exercise={exercises2}/>
+      <Content part={part3} exercise={exercises3}/>
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
