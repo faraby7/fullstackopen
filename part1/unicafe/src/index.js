eb2c3839f78b12c6989aec181a4efa2956c1d345
Youssef Faraby <yusuf.faraby@gmail.com>
2020-12-09 13:23:03 +0200

    1.7: unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,15 +1,45 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+export const GOODSCORE    = 1
+export const NEUTRALSCORE = 0
+export const BADSCORE     = -1
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const totalNumberStatistic = bad + neutral + good
+
+  const averageScoreCalculator = (good, neutral, bad) => {
+
+    const goodScore     = good * GOODSCORE
+    const neutralScore  = neutral * NEUTRALSCORE
+    const badScore      = bad * BADSCORE
+
+    const totalScore    = bad + neutral + good
+    if(totalScore === 0){
+      return 0
+    }
+    return (badScore + neutralScore + goodScore) / totalScore
+  }
+
+  const positiveScoreCalculator = (good, neutral, bad) => {
+    const totalScore    = bad + neutral + good
+    if(totalScore === 0){
+      return 0
+    }
+    return good*100/ totalScore
+  }
+
+  const positiveScoreStatistic = positiveScoreCalculator(good, neutral, bad)
+  const averageScoreStatistic = averageScoreCalculator(good, neutral, bad)
+
+
 
   return (
     <div>
-
       <h1>Give feedback</h1>
       <button onClick={()=> setGood(good + 1)}>good</button>
       <button onClick={()=> setNeutral(neutral + 1)}>neutral</button>
@@ -18,6 +48,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {totalNumberStatistic}</p>
+      <p>average {averageScoreStatistic}</p>
+      <p>positive {positiveScoreStatistic} %</p>
     </div>
   )
 }
